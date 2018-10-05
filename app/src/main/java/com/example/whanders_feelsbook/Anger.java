a7d0c9170e7358e0bb99ee31ef81013d8dce46e2
whander <35234387+whander@users.noreply.github.com>
2018-10-05 11:49:21 -0600

    Update Anger.java

--- app/src/main/java/com/example/whanders_feelsbook/Anger.java
+++ app/src/main/java/com/example/whanders_feelsbook/Anger.java
@@ -2,6 +2,9 @@ package com.example.whanders_feelsbook;
 
 import android.util.Log;
 
+//Purpose is to be a subclass of mood that sets type to anger.
+//No known issues
+
 public class Anger extends Mood {
     //only difference in the mood subclasses is that they initialize the type
 
