249207aefe5efc8a2c8823eeb9ccbdc6a559650a
whander <35234387+whander@users.noreply.github.com>
2018-10-05 13:41:24 -0600

    Update Love.java

--- app/src/main/java/com/example/whanders_feelsbook/Love.java
+++ app/src/main/java/com/example/whanders_feelsbook/Love.java
@@ -2,6 +2,9 @@ package com.example.whanders_feelsbook;
 
 import android.util.Log;
 
+//Purpose is to be a subclass of mood that sets type to love.
+//No known issues
+
 public class Love extends Mood {
     private String name = "Love";
 
