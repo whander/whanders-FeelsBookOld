0d31a38a663b011310abd4afb8207a08bf32f04c
whander <35234387+whander@users.noreply.github.com>
2018-10-05 13:44:01 -0600

    Update PostTooLongException.java

--- app/src/main/java/com/example/whanders_feelsbook/PostTooLongException.java
+++ app/src/main/java/com/example/whanders_feelsbook/PostTooLongException.java
@@ -1,6 +1,7 @@
 package com.example.whanders_feelsbook;
 
 //taken from lonelyTwitter lab
+//Purpose of the class is to raise an error if posts are > 100 characters
 
 public class PostTooLongException extends RuntimeException {
 
