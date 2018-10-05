ce20860a3d0310f890fbb3422df12ee628c7a310
whander <35234387+whander@users.noreply.github.com>
2018-10-05 12:52:52 -0600

    Update Joy.java

--- app/src/main/java/com/example/whanders_feelsbook/Joy.java
+++ app/src/main/java/com/example/whanders_feelsbook/Joy.java
@@ -2,6 +2,9 @@ package com.example.whanders_feelsbook;
 
 import android.util.Log;
 
+//Purpose is to be a subclass of mood that sets type to joy.
+//No known issues
+
 public class Joy extends Mood {
     private String name = "Joy";
 
